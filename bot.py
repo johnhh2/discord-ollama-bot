9a1fd34cc93f27cc6a60844a28e60e40946ac1f9
johnhh2 <johnhh2@users.noreply.github.com>
2026-04-05 20:01:36 -0500

    remove dot env

--- bot.py
+++ bot.py
@@ -12,8 +12,6 @@ from discord import ui
 from dotenv import load_dotenv
 from collections import defaultdict, deque
 
-load_dotenv()
-
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 OLLAMA_BASE_URL = os.getenv("OLLAMA_BASE_URL", "http://host.docker.internal:11434")
 OLLAMA_MODEL = os.getenv("OLLAMA_MODEL", "dolphin3:8b")
